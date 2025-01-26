98c7e3f7e453daea5fe6ab85db8d35aada79fd83
Promise Ogbonna <promise.bones@gmail.com>
2025-01-26 11:00:40 +0300

    Remove hardcoded example and update main script to use dynamic inputs

--- app/main.py
+++ app/main.py
@@ -1,18 +1,18 @@
 import asyncio
+import json
 
 from app.agent.main import WebExplorerAgent
 from app.logger import base_logger
 
-logger = base_logger.getChild(__name__)
-
-
 if __name__ == "__main__":
-    prompt = "You are web explorer agent"
-    title = "Get the latest weather information about Nairobi"
-    instruction = "Open google.com and get the latest weather information about Nairobi, Kenya"
+    logger = base_logger.getChild(__name__)
+
+    prompt = ""
+    title = ""
+    instruction = ""
 
     agent = WebExplorerAgent()
     output = asyncio.run(
         agent.run(prompt=prompt, title=title, instruction=instruction),
     )
-    logger.info(output)
+    logger.info(json.dumps(output, indent=4))
